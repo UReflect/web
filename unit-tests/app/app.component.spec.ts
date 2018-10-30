711f4246b6111083aeea0fd3bc7f8fac6d51a44c
Aurelien ARINO <aurelien.arino@mythalesgroup.io>
2018-10-30 10:29:30 +0100

    build(circleci): Added Tests + exclude develop branch

--- unit-tests/app/app.component.spec.ts
+++ unit-tests/app/app.component.spec.ts
@@ -1,20 +1,20 @@
-import { TestBed, async } 	from '@angular/core/testing'
-import { AppComponent }   	from '../../src/app/app.component'
-import { AppModule }      	from '../../src/app/app.module'
-import { RouterTestingModule } 	from '@angular/router/testing'
+import { TestBed, async }      from '@angular/core/testing';
+import { AppComponent }        from '../../src/app/app.component';
+import { AppModule }           from '../../src/app/app.module';
+import { RouterTestingModule } from '@angular/router/testing';
 
 describe('AppComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       imports: [
         AppModule,
-	RouterTestingModule
+        RouterTestingModule
       ]
-    }).compileComponents()
-  }))
+    }).compileComponents();
+  }));
   it('should create the app', async(() => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
     expect(app).toBeTruthy();
   }));
-})
+});
