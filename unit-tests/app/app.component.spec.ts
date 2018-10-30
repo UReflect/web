3a6cd58ceeb2f9f67dcfe03390429f3b139891ba
Aurelien ARINO <aurelien.arino@mythalesgroup.io>
2018-10-30 10:21:07 +0100

    test(app): Fixed tests

--- unit-tests/app/app.component.spec.ts
+++ unit-tests/app/app.component.spec.ts
@@ -1,13 +1,20 @@
-import { TestBed, async } from '@angular/core/testing'
-import { AppComponent }   from '../../src/app/app.component'
-import { AppModule }      from '../../src/app/app.module'
+import { TestBed, async } 	from '@angular/core/testing'
+import { AppComponent }   	from '../../src/app/app.component'
+import { AppModule }      	from '../../src/app/app.module'
+import { RouterTestingModule } 	from '@angular/router/testing'
 
 describe('AppComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       imports: [
-        AppModule
+        AppModule,
+	RouterTestingModule
       ]
     }).compileComponents()
   }))
+  it('should create the app', async(() => {
+    const fixture = TestBed.createComponent(AppComponent);
+    const app = fixture.debugElement.componentInstance;
+    expect(app).toBeTruthy();
+  }));
 })
